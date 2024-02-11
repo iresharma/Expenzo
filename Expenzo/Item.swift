7b37e8407d244166372e80dd1d186fa10a470baa
iresharma <iresh.sharma8@gmail.com>
2024-02-11 19:46:29 +0530

    Initial Commit

---
+++ Expenzo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Expenzo
+//
+//  Created by Iresh Sharma on 11/02/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
