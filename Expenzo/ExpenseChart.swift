e54217c86409585c7853f2cd93e6266e72c4a752
iresharma <iresh.sharma8@gmail.com>
2024-02-12 19:21:42 +0530

    Started with swiftui , made basic changes to make a starter UI

---
+++ Expenzo/ExpenseChart.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseChart.swift
+//  Expenzo
+//
+//  Created by Iresh Sharma on 12/02/24.
+//
+
+import SwiftUI
+
+struct ExpenseChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseChart()
+}
---
+++ Expenzo/SpendBarChart.swift
@@ -0,0 +1,18 @@
+//
+//  SpendBarChart.swift
+//  Expenzo
+//
+//  Created by Iresh Sharma on 12/02/24.
+//
+
+import SwiftUI
+
+struct SpendBarChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpendBarChart()
+}
