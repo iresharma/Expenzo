308c56cb79b50549b72d8b175d86d743c09e3a82
iresharma <iresh.sharma8@gmail.com>
2024-02-16 03:51:24 +0530

    Fixed fonts on UI and added transactions page UI

---
+++ Expenzo/TransactionListItem.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionListItem.swift
+//  Expenzo
+//
+//  Created by Iresh Sharma on 16/02/24.
+//
+
+import SwiftUI
+
+struct TransactionListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionListItem()
+}
---
+++ Expenzo/Transactions.swift
@@ -0,0 +1,18 @@
+//
+//  Transactions.swift
+//  Expenzo
+//
+//  Created by Iresh Sharma on 16/02/24.
+//
+
+import SwiftUI
+
+struct Transactions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Transactions()
+}
